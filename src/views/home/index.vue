a5ffd653c66c45c5a161a91fce4e7c06a1f39991
zhaoyx <zhaoyunxinhh@163.com>
2019-06-26 20:13:09 +0800

    调整目录&调整页面

---
+++ src/views/home/index.vue
@@ -0,0 +1,12 @@
+<template>
+   <div>
+     <p>主页</p>
+   </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AppHome'
+}
+</script>
---
+++ src/views/login/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>登录路由</p>
+</template>
+
+<script>
+export default {
+  name: 'AppLogin'
+}
+</script>
+
+<style>
+
+</style>
